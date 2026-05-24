f51e31dd3f8cb8844709fe1e059bb121332c31e8
Geovany <geovanyzambon97@gmail.com>
2026-05-24 10:29:59 +0530

    Atualiza testes da Home

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,10 +1,22 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../app/page'
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
 
-describe('Home Page', () => {
-  it('renders correctly', () => {
-    render(<Home />)
+describe("Home Page", () => {
+  it("renders correctly", () => {
+    render(<Home />);
 
-    expect(screen.getByText(/Get started/i)).toBeInTheDocument()
-  })
-})
+    expect(
+      screen.getByRole("heading", {
+        name: /Projeto CI\/CD com Next.js/i,
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole("link", {
+        name: /Ver Repositório/i,
+      })
+    ).toBeInTheDocument();
+
+    expect(screen.getAllByText(/GitHub Actions/i).length).toBeGreaterThan(0);
+  });
+});
